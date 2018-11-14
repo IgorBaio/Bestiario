84e997e8b145b963377a62da4ad501e9f13f53eb
IgorBaio <igorbaiosoares@gmail.com>
2018-11-14 20:02:06 -0200

    bestiario

---
+++ Segundo/script.js
@@ -0,0 +1,352 @@
+function link() {
+    window.location = "criaturas.html";
+
+}
+
+function carregaUm() {
+    var som = new Audio();
+    som.src = "sons/botaoCriat.mp3";
+    som.play();
+
+
+}
+
+function link1() {
+    window.location = "Lobisomem.html";
+
+}
+
+function link2() {
+    window.location = "wen2.html";
+
+}
+function link3() {
+    window.location = "Bruxa.html";
+
+}
+function link4() {
+    window.location = "Demonio.html";
+
+}
+function link5() {
+    window.location = "Fantasma.html";
+
+}
+
+function Home() {
+    window.location = "index.html";
+
+}
+
+function uivo() {
+    var som = new Audio();
+    som.src = "sons/uivo1.mp3";
+    som.play();
+
+
+}
+
+function moveWend() {
+    var corpo = document.getElementById("corpo");
+    var caracteristica = document.getElementById("caract");
+    var fraqueza = document.getElementById("fraq");
+    var paragrafo = document.getElementById("parag");
+
+    caracteristica.style.backgroundColor = "rgba(245, 222, 179, 0.6)";
+    caracteristica.style.color = "rgb(10, 23, 55)";
+    caracteristica.style.fontStyle = "italic";
+    caracteristica.style.fontWeight = "bolder";
+
+    corpo.style.backgroundImage = "url(img/wendigo2.png)";
+
+
+    fraqueza.style.backgroundColor = "transparent";
+    fraqueza.style.color = "transparent";
+    fraqueza.style.fontStyle = "normal";
+    fraqueza.style.fontWeight = "normal";
+
+    paragrafo.style.backgroundColor = "transparent";
+    paragrafo.style.color = "transparent";
+    paragrafo.style.fontStyle = "normal";
+    paragrafo.style.fontWeight = "normal";
+
+}
+
+function moveWend1() {
+    var corpo = document.getElementById("corpo");
+    var caracteristica = document.getElementById("caract");
+    var fraqueza = document.getElementById("fraq");
+    var paragrafo = document.getElementById("parag");
+
+    fraqueza.style.backgroundColor = "rgba(245, 222, 179, 0.6)";
+    fraqueza.style.color = "rgb(10, 23, 55)";
+    fraqueza.style.fontStyle = "italic";
+    fraqueza.style.fontWeight = "bolder";
+
+    corpo.style.backgroundImage = "url(img/wendigo.jpg)";
+
+
+    caracteristica.style.backgroundColor = "transparent";
+    caracteristica.style.color = "transparent";
+    caracteristica.style.fontStyle = "normal";
+    caracteristica.style.fontWeight = "normal";
+
+    paragrafo.style.backgroundColor = "transparent";
+    paragrafo.style.color = "transparent";
+    paragrafo.style.fontStyle = "normal";
+    paragrafo.style.fontWeight = "normal";
+
+}
+
+function moveWend2() {
+    var corpo = document.getElementById("corpo");
+    var caracteristica = document.getElementById("caract");
+    var fraqueza = document.getElementById("fraq");
+    var paragrafo = document.getElementById("parag");
+
+    caracteristica.style.backgroundColor = "transparent";
+    caracteristica.style.color = "transparent";
+    caracteristica.style.fontStyle = "normal";
+    caracteristica.style.fontWeight = "normal";
+    fraqueza.style.backgroundColor = "transparent";
+    fraqueza.style.color = "transparent";
+    fraqueza.style.fontStyle = "normal";
+    fraqueza.style.fontWeight = "normal";
+
+    corpo.style.backgroundImage = "url(img/wendigo.gif)";
+
+    paragrafo.style.backgroundColor = "rgba(245, 222, 179, 0.6)";
+    paragrafo.style.color = "rgb(10, 23, 55)";
+    paragrafo.style.fontStyle = "italic";
+    paragrafo.style.fontWeight = "bolder";
+
+}
+
+function moveWend3(){
+ var corpo = document.getElementById("corpo");
+    var caracteristica = document.getElementById("caract");
+    var fraqueza = document.getElementById("fraq");
+    var paragrafo = document.getElementById("parag");
+
+    caracteristica.style.backgroundColor = "transparent";
+    caracteristica.style.color = "transparent";
+    caracteristica.style.fontStyle = "normal";
+    caracteristica.style.fontWeight = "normal";
+    fraqueza.style.backgroundColor = "transparent";
+    fraqueza.style.color = "transparent";
+    fraqueza.style.fontStyle = "normal";
+    fraqueza.style.fontWeight = "normal";
+
+    corpo.style.backgroundImage = "url(img/wendigo.gif)";
+
+    paragrafo.style.backgroundColor = "transparent";
+    paragrafo.style.color = "transparent";
+    paragrafo.style.fontStyle = "normal";
+    paragrafo.style.fontWeight = "normal";
+
+}
+
+function moveDemo() {
+    var corpo = document.getElementById("corpo");
+    var caracteristica = document.getElementById("caract");
+    var fraqueza = document.getElementById("fraq");
+    var paragrafo = document.getElementById("parag");
+
+    caracteristica.style.backgroundColor = "rgba(245, 222, 179, 0.6)";
+    caracteristica.style.color = "rgb(10, 23, 55)";
+    caracteristica.style.fontStyle = "italic";
+    caracteristica.style.fontWeight = "bolder";
+
+    corpo.style.backgroundImage = "url(img/demo.jpg)";
+
+
+    fraqueza.style.backgroundColor = "transparent";
+    fraqueza.style.color = "transparent";
+    fraqueza.style.fontStyle = "normal";
+    fraqueza.style.fontWeight = "normal";
+
+    paragrafo.style.backgroundColor = "transparent";
+    paragrafo.style.color = "transparent";
+    paragrafo.style.fontStyle = "normal";
+    paragrafo.style.fontWeight = "normal";
+
+}
+
+function moveDemo1() {
+    var corpo = document.getElementById("corpo");
+    var caracteristica = document.getElementById("caract");
+    var fraqueza = document.getElementById("fraq");
+    var paragrafo = document.getElementById("parag");
+
+    fraqueza.style.backgroundColor = "rgba(245, 222, 179, 0.6)";
+    fraqueza.style.color = "rgb(10, 23, 55)";
+    fraqueza.style.fontStyle = "italic";
+    fraqueza.style.fontWeight = "bolder";
+
+    corpo.style.backgroundImage = "url(img/demo3.jpg)";
+
+
+    caracteristica.style.backgroundColor = "transparent";
+    caracteristica.style.color = "transparent";
+    caracteristica.style.fontStyle = "normal";
+    caracteristica.style.fontWeight = "normal";
+
+    paragrafo.style.backgroundColor = "transparent";
+    paragrafo.style.color = "transparent";
+    paragrafo.style.fontStyle = "normal";
+    paragrafo.style.fontWeight = "normal";
+
+}
+
+function moveDemo2() {
+    var corpo = document.getElementById("corpo");
+    var caracteristica = document.getElementById("caract");
+    var fraqueza = document.getElementById("fraq");
+    var paragrafo = document.getElementById("parag");
+
+    caracteristica.style.backgroundColor = "transparent";
+    caracteristica.style.color = "transparent";
+    caracteristica.style.fontStyle = "normal";
+    caracteristica.style.fontWeight = "normal";
+    fraqueza.style.backgroundColor = "transparent";
+    fraqueza.style.color = "transparent";
+    fraqueza.style.fontStyle = "normal";
+    fraqueza.style.fontWeight = "normal";
+
+    corpo.style.backgroundImage = "url(img/zoio.jpg)";
+
+    paragrafo.style.backgroundColor = "rgba(245, 222, 179, 0.6)";
+    paragrafo.style.color = "rgb(10, 23, 55)";
+    paragrafo.style.fontStyle = "italic";
+    paragrafo.style.fontWeight = "bolder";
+
+}
+
+function moveDemo3(){
+ var corpo = document.getElementById("corpo");
+    var caracteristica = document.getElementById("caract");
+    var fraqueza = document.getElementById("fraq");
+    var paragrafo = document.getElementById("parag");
+
+    caracteristica.style.backgroundColor = "transparent";
+    caracteristica.style.color = "transparent";
+    caracteristica.style.fontStyle = "normal";
+    caracteristica.style.fontWeight = "normal";
+    fraqueza.style.backgroundColor = "transparent";
+    fraqueza.style.color = "transparent";
+    fraqueza.style.fontStyle = "normal";
+    fraqueza.style.fontWeight = "normal";
+
+    corpo.style.backgroundImage = "url(img/demo8.gif)";
+
+    paragrafo.style.backgroundColor = "transparent";
+    paragrafo.style.color = "transparent";
+    paragrafo.style.fontStyle = "normal";
+    paragrafo.style.fontWeight = "normal";
+
+}
+
+/*function fecharJanela(){
+        var Hellowww =document.getElementById("body").src="img/demo7.gif";
+         alert(Hellowww);  
+    }  */
+
+function moveGhost() {
+    var corpo = document.getElementById("corpo");
+    var caracteristica = document.getElementById("caract");
+    var fraqueza = document.getElementById("fraq");
+    var paragrafo = document.getElementById("parag");
+
+    caracteristica.style.backgroundColor = "rgba(245, 222, 179, 0.6)";
+    caracteristica.style.color = "rgb(10, 23, 55)";
+    caracteristica.style.fontStyle = "italic";
+    caracteristica.style.fontWeight = "bolder";
+
+    corpo.style.backgroundImage = "url(img/uuuuhhhh.gif)";
+
+
+    fraqueza.style.backgroundColor = "transparent";
+    fraqueza.style.color = "transparent";
+    fraqueza.style.fontStyle = "normal";
+    fraqueza.style.fontWeight = "normal";
+
+    paragrafo.style.backgroundColor = "transparent";
+    paragrafo.style.color = "transparent";
+    paragrafo.style.fontStyle = "normal";
+    paragrafo.style.fontWeight = "normal";
+
+}
+
+function moveGhost1() {
+    var corpo = document.getElementById("corpo");
+    var caracteristica = document.getElementById("caract");
+    var fraqueza = document.getElementById("fraq");
+    var paragrafo = document.getElementById("parag");
+
+    fraqueza.style.backgroundColor = "rgba(245, 222, 179, 0.6)";
+    fraqueza.style.color = "rgb(10, 23, 55)";
+    fraqueza.style.fontStyle = "italic";
+    fraqueza.style.fontWeight = "bolder";
+
+    corpo.style.backgroundImage = "url(img/ghostin1.gif)";
+
+
+    caracteristica.style.backgroundColor = "transparent";
+    caracteristica.style.color = "transparent";
+    caracteristica.style.fontStyle = "normal";
+    caracteristica.style.fontWeight = "normal";
+
+    paragrafo.style.backgroundColor = "transparent";
+    paragrafo.style.color = "transparent";
+    paragrafo.style.fontStyle = "normal";
+    paragrafo.style.fontWeight = "normal";
+
+}
+
+function moveGhost2() {
+    var corpo = document.getElementById("corpo");
+    var caracteristica = document.getElementById("caract");
+    var fraqueza = document.getElementById("fraq");
+    var paragrafo = document.getElementById("parag");
+
+    caracteristica.style.backgroundColor = "transparent";
+    caracteristica.style.color = "transparent";
+    caracteristica.style.fontStyle = "normal";
+    caracteristica.style.fontWeight = "normal";
+    fraqueza.style.backgroundColor = "transparent";
+    fraqueza.style.color = "transparent";
+    fraqueza.style.fontStyle = "normal";
+    fraqueza.style.fontWeight = "normal";
+
+    corpo.style.backgroundImage = "url(img/ghostin2.gif)";
+
+    paragrafo.style.backgroundColor = "rgba(245, 222, 179, 0.6)";
+    paragrafo.style.color = "rgb(10, 23, 55)";
+    paragrafo.style.fontStyle = "italic";
+    paragrafo.style.fontWeight = "bolder";
+
+}
+
+function moveGhost3(){
+ var corpo = document.getElementById("corpo");
+    var caracteristica = document.getElementById("caract");
+    var fraqueza = document.getElementById("fraq");
+    var paragrafo = document.getElementById("parag");
+
+    caracteristica.style.backgroundColor = "transparent";
+    caracteristica.style.color = "transparent";
+    caracteristica.style.fontStyle = "normal";
+    caracteristica.style.fontWeight = "normal";
+    fraqueza.style.backgroundColor = "transparent";
+    fraqueza.style.color = "transparent";
+    fraqueza.style.fontStyle = "normal";
+    fraqueza.style.fontWeight = "normal";
+
+    corpo.style.backgroundImage = "url(img/ghostin.gif)";
+
+    paragrafo.style.backgroundColor = "transparent";
+    paragrafo.style.color = "transparent";
+    paragrafo.style.fontStyle = "normal";
+    paragrafo.style.fontWeight = "normal";
+
+}
